fc4a33ed6a1b95c7e53d92098a2aa7feee18447e
roxanmlr <roxanmlr@gmail.com>
2026-06-10 18:35:16 +0200

    format : make f

--- src/http/HttpResponse.hpp
+++ src/http/HttpResponse.hpp
@@ -15,22 +15,21 @@
 #include "IHttpResponse.hpp"
 #include <map>
 
-class HttpResponse : public IHttpResponse
-{
-	private:
-		int _status;
-		std::map<std::string, std::string> _header;
-		std::string _body;
-		std::string _getCodeError(int code)const;
+class HttpResponse : public IHttpResponse {
+private:
+	int								   _status;
+	std::map<std::string, std::string> _header;
+	std::string						   _body;
+	std::string						   _getCodeError(int code) const;
 
-	public:
-		virtual ~HttpResponse(void);
-		HttpResponse(void);
-		HttpResponse(const HttpResponse & base);
-		HttpResponse &operator=(const HttpResponse & base);
+public:
+	virtual ~HttpResponse(void);
+	HttpResponse(void);
+	HttpResponse(const HttpResponse& base);
+	HttpResponse&		operator=(const HttpResponse& base);
 
-		virtual void setStatus(int code);
-		virtual void setHeader(const std::string & name, const std::string & value);
-		virtual void setBody(const std::string & body);
-		virtual std::string serialize()const;
+	virtual void		setStatus(int code);
+	virtual void		setHeader(const std::string& name, const std::string& value);
+	virtual void		setBody(const std::string& body);
+	virtual std::string serialize() const;
 };
