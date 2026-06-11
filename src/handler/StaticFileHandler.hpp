9702b917547be6dd717e78bb1fb8dfdcb939ec59
Mehdi Zouhir <mzouhir@f6r8s10.clusters.42paris.fr>
2026-06-11 15:51:06 +0200

    ok

---
+++ src/handler/StaticFileHandler.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   StaticFileHandler.hpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzouhir <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/10 15:28:25 by mzouhir           #+#    #+#             */
+/*   Updated: 2026/06/11 14:22:18 by mzouhir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "IRequestHandler.hpp"
+
+class StaticFileHandler : public IRequestHandler
+{
+	private:
+
+	public:
+
+		virtual ~StaticFileHandler();
+		StaticFileHandler();
+		StaticFileHandler(const StaticFileHandler & base);
+		StaticFileHandler &operator=(const StaticFileHandler & base);
+
+		virtual bool canHandle(const IHttpRequest& req, const ILocationConfig & loc);
+		virtual bool handle(const IHttpRequest& req, const ILocationConfig & loc, IHttpResponse & res);
+
+};
